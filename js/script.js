e90ddee7aceafd1177bedaaa326e0af8305f9bfd
aleksasavic1 <asavic846@gmail.com>
2024-12-12 22:50:11 +0100

    Update v1.1

--- js/script.js
+++ js/script.js
@@ -12,6 +12,49 @@ function menu() {
             menuLinks.style.transform = 'translateX(0%)';
         }, 50);
 
+
+        let links = document.querySelectorAll("#menuLinks li a");
+
+        // ABOUT ME
+        links[0].addEventListener("click", (e) => {
+            e.preventDefault();
+            window.location.href = "#secondPage";
+            menuLinks.style.transform = 'translateX(100%)';
+            setTimeout(() => {
+                menuLinks.style.display = 'none';
+            }, 500);
+        });
+
+        // SKILLS
+        links[1].addEventListener("click", (e) => {
+            e.preventDefault();
+            window.location.href = "#thirdPage";
+            menuLinks.style.transform = 'translateX(100%)';
+            setTimeout(() => {
+                menuLinks.style.display = 'none';
+            }, 500);
+        });
+
+        // GITHUB ACCOUNT
+        links[2].addEventListener("click", (e) => {
+            e.preventDefault();
+            window.location.href = "#fourthPage";
+            menuLinks.style.transform = 'translateX(100%)';
+            setTimeout(() => {
+                menuLinks.style.display = 'none';
+            }, 500);
+        });
+
+        // COURSES
+        links[3].addEventListener("click", (e) => {
+            e.preventDefault();
+            window.location.href = "#fifthPage";
+            menuLinks.style.transform = 'translateX(100%)';
+            setTimeout(() => {
+                menuLinks.style.display = 'none';
+            }, 500);
+        });
+
     } else {
         menuLinks.style.transform = 'translateX(100%)';
         setTimeout(() => {
@@ -21,4 +64,30 @@ function menu() {
 }
 
 
-let header = document.querySelector("header");
+
+
+let agesSpan = document.querySelector("#ages");
+let birthDate = new Date(2003, 8, 22);
+let currentDate = new Date();
+
+let ages = currentDate.getFullYear() - birthDate.getFullYear();
+
+agesSpan.innerText = ages;
+agesSpan.style.color = "#FF000D";
+
+
+
+
+let animWords = document.querySelector(".animWords").innerText;
+
+let wordsArray = animWords.split("");
+let singleSpan = "";
+
+wordsArray.forEach((word, index) => {
+    let span = document.createElement("span");
+    span.textContent = word;
+    span.style.animationDelay = `${index * 0.2}s`;
+    singleSpan += span.outerHTML;
+});
+
+document.querySelector(".animWords").innerHTML = singleSpan;
